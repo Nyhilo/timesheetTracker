74e66763e4b2c6ed273f0387772da37fe54e11b8
nyhilo <silentclowd@gmail.com>
2016-11-30 17:09:33 -0700

    Fixed the formatting on hour/minute/month/day feilds to put a 0 in front of the month if it's a single digit number

--- timesheettracker.py
+++ timesheettracker.py
@@ -23,6 +23,9 @@ def time(t):
         hour = str(t.tm_hour-12)
         ampm = "pm"
 
+    if len(hour) < 2: hour = "0" + hour
+    if len(minute) < 2: minute = "0" + minute
+
     return hour + ":" + minute + ampm
 
 def date(t):
@@ -33,20 +36,24 @@ def date(t):
     month = str(t.tm_mon)
     day = str(t.tm_mday)
     year = str(t.tm_year)
+
+    if len(month) < 2: month = "0" + month
+    if len(day) < 2: day = "0" + day
+
     return month + "/" + day + "/" + year
 
 
 #main
 def main():
-    input("Press enter to clock in...")
+    # input("Press enter to clock in...")
     clockInTime = localtime()
     print("You're clocked in at " + time(clockInTime)
             + " on " + date(clockInTime) + ".")
-    input("Press enter to clock out...")
+    # input("Press enter to clock out...")
     clockOutTime = localtime()
     print("You're clocked out at " + time(clockOutTime)
             + " on " + date(clockOutTime) + ". Have a nice day!")
-    input() # Wait for the user to close the window
+    # input() # Wait for the user to close the window
 
     # Write the line to the csv file.
     with open('timesheet.csv', 'a', newline='') as f:
